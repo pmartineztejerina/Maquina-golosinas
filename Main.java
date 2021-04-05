bf31961217f15d70c0add5b506b730c7772d9868
Pilar Martinez Tejerina <pmartineztejerina@gmail.com>
2021-04-05 17:12:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,9 +8,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//ImportarTute1 importaTute1=new ImportarTute1();
-		//ImportarTute2 importaTute2=new ImportarTute2();
-		//ImportarCuadradoMagico importarCuadradoMagico=new ImportarCuadradoMagico();
 		MaquinaGolosinas maquinaGolosinas=new MaquinaGolosinas();
 		
 		
